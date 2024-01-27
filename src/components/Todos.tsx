3a79c0cba9c271e7de67daa6e9262ce1fee95f10
Harish1651999 <hk1651999@gmail.com>
2024-01-27 18:55:00 +0530

    add props and type notation.

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-const Todos = () => {
+const Todos: React.FC<{items: string[]}> = (props) => {
   return (
     <ul>
-        <li>Learn React</li>
-        <li>Learn TypeScript</li>
+        {props.items.map(item => <li key={item}>{item}</li>)}
     </ul>
   )
 }
